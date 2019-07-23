4ccf166f5f66d22b4df7fa23cbe3f14613e54ddd
Mryan <40586009+Mryan2005@users.noreply.github.com>
2019-07-23 20:35:07 +0800

    fix bugs

--- core/function.php
+++ core/function.php
@@ -1,20 +1,19 @@
 <?php
-/*
+
 function jump($page,$get_action,$count_get_action){
     $times = 0;
     foreach ($get_action as $i){
         if($page && $i) {
-            
+            include($page.'.php');
         } else {
             $times++;
             if($times >= $count_get_action){
-                error
+                error('No found the page or the page is a error.');
             } 
         }
     }
-
 }
-*/
+
 function error($sthing){
     if(count($sthing) == 1){
         echo '<p>
