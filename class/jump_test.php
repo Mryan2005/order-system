285e7367d48a6c315d112c2038575ba6c4c113fe
Mryan <40586009+Mryan2005@users.noreply.github.com>
2019-07-24 15:33:09 +0800

    fix bugs

--- class/jump_test.php
+++ class/jump_test.php
@@ -12,15 +12,4 @@ class jumpTest extends PHPUnit_Framework_TestCase {
 		$jump = new jump;
         $actual = $jump->jump($get_actions, $get_action_list,$count_get_action);
 	}
-	function testLoginFail() {
-		$get_actions = 'home';
-        $get_action_list = array(
-            'admin',
-            'login',
-            'order',
-        );
-        $count_get_action = count($get_action_list);
-		$jump = new jump;
-        $actual = $jump->jump($get_actions, $get_action_list,$count_get_action);
-	}
 }
