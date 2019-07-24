8861c2ba0d79b026b8d278cf7b6e9c9b061cb496
Mryan <40586009+Mryan2005@users.noreply.github.com>
2019-07-24 16:12:22 +0800

    fix bugs

--- class/jump_class.php
+++ class/jump_class.php
@@ -2,14 +2,9 @@
 class jump {
     public function jump($get_actions = '',$get_action_list = '',$count_get_action = ''){
         if($get_actions != ''){
-            $times = 0;
             foreach($get_action_list as $i) {
-                $times++;
                 if($get_actions == $i) {
                     include($i.'.php');
-                } else if($times >= $count_get_action) {
-                    echo '<h4>we will help you go back</h4>
-                    <a href="index.php">go back</a>';
                 }
             }
         } 
